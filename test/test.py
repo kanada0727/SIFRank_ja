c8ad1d12201f15bfee137fc49796d5be963932b5
tanajp <50686226+tanajp@users.noreply.github.com>
2020-08-18 00:58:28 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,13 @@
 import sys
-sys.path.append('/content/drive/My Drive/SIFRank')
-sys.path.append('/content/drive/My Drive/SIFRank/embeddings')
+sys.path.append('/content/drive/My Drive/SIFRank_ja_model')
+sys.path.append('/content/drive/My Drive/SIFRank_ja_model/embeddings')
 import stanza
 import sent_emb_sif, word_emb_elmo
 from model.method import SIFRank, SIFRank_plus
 
 #download from https://allennlp.org/elmo
 options_file = "https://exawizardsallenlp.blob.core.windows.net/data/options.json"
-weight_file = "/content/drive/My Drive/SIFRank/auxiliary_data/weights.hdf5"
+weight_file = "/content/drive/My Drive/SIFRank_ja_model/auxiliary_data/weights.hdf5"
 
 ELMO = word_emb_elmo.WordEmbeddings(options_file, weight_file, cuda_device=0)
 SIF = sent_emb_sif.SentEmbeddings(ELMO, lamda=1.0)
@@ -17,8 +17,8 @@ ja_model = stanza.Pipeline(
 elmo_layers_weight = [0.0, 1.0, 0.0]
 
 text = "ここにテキストを入力してください。"
-keyphrases = SIFRank(text, SIF, ja_model, N=15,elmo_layers_weight=elmo_layers_weight)
-keyphrases_ = SIFRank_plus(text, SIF, ja_model, N=15, elmo_layers_weight=elmo_layers_weight)
+keyphrases = SIFRank(text, SIF, ja_model, N=5,elmo_layers_weight=elmo_layers_weight)
+keyphrases_ = SIFRank_plus(text, SIF, ja_model, N=5, elmo_layers_weight=elmo_layers_weight)
 
 print(keyphrases)
 print(keyphrases_)
